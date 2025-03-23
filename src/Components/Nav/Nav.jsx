3a77e8f93a1383d9b4808bc203aee51848510a06
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-03-23 15:57:59 +1100

    login page added to nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import './Nav.css'
-
+import React from "react";
+import "./Nav.css";
 
 const Nav = () => {
     return (
         <navbar className="navbar">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/Contact">Contact</a></li>
-                <li><a href="/favourites">Favourites</a></li>
-                <li><a>Contact</a></li>
-                <li><a>Posts</a></li>
-                <li className="nav-right"><a>Account</a></li>
-                
-
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/Contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/favourites">Favourites</a>
+                </li>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+                <li className="nav-right">
+                    <a>Account</a>
+                </li>
             </ul>
         </navbar>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
