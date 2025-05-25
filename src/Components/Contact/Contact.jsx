aede60d5e1a24f0f509becbf37eaa12ab1dec392
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-05-25 16:24:14 +1000

    add contact success message

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Contact.css";
 
 const Contact = () => {
+  const [success, setSuccess] = useState("");
+  const [submission, setSubmission] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -22,6 +29,12 @@ const Contact = () => {
 
     if (res.success) {
       console.log("Success", res);
+      setSuccess("Thank you, your message has been sent!");
+      setSubmission({
+        name: "",
+        email: "",
+        message: ""
+      })
     }
   };
 
@@ -36,6 +49,13 @@ const Contact = () => {
             className="field"
             placeholder="Enter your Name"
             name="name"
+            value={submission.name}
+            onChange={(e) =>
+              setSubmission({
+                ...submission,
+                name: e.target.value
+              })
+            }
             required
           />
         </div>
@@ -46,20 +66,35 @@ const Contact = () => {
             className="field"
             placeholder="Enter your Email"
             name="email"
+            value={submission.email}
+            onChange={(e) =>
+              setSubmission({
+                ...submission,
+                email: e.target.value
+              })
+            }
             required
           />
         </div>
         <div className="input-box">
-          <label>Full Name</label>
+          <label>Message</label>
           <textarea
             name="message"
             id=""
             className="field mess"
             placeholder="Enter your message"
+            value={submission.message}
+            onChange={(e) =>
+              setSubmission({
+                ...submission,
+                message: e.target.value
+              })
+            }
             required
           ></textarea>
         </div>
         <button type="submit">Send Message</button>
+        <p className="success">{success}</p>
       </form>
     </section>
   );
